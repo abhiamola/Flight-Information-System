0b97bfa88129e842cdfa703e22f6d1ad4da152df
plsakr <39004027+plsakr@users.noreply.github.com>
2022-11-13 16:45:09 -0500

    added frontend styling, and updated documents

--- backend/data/gateway.js
+++ backend/data/gateway.js
@@ -1,4 +1,13 @@
 import mysql2 from 'mysql2/promise';
+
+const FLIGHT_QUERY = `SELECT * from realTimeFlightData as r inner join airlines a on r.airline_icao = a.icao_code
+                                                                             inner join airports arrival on r.arr_icao = arrival.icao_code
+                                                                             inner join airports departure on r.dep_icao = departure.icao_code
+                                                                             inner join countries cArr on arrival.country_code = cArr.code
+                                                                             inner join countries cDep on departure.country_code = cDep.code`;
+
+const AIRLINE_QUERY = `SELECT * FROM airlines`;
+
 export class DataGateway {
     
     #connectionParams;
@@ -20,12 +29,13 @@ export class DataGateway {
     // ===========
 
     getAirlines(offset, limit) {
-        return this.#getDBConnection().then(conn => conn.query(`SELECT * FROM airlines LIMIT ? OFFSET ?`, [parseInt(limit,10), parseInt(offset,10)]))
+
+        return this.#getDBConnection().then(conn => conn.query(`${AIRLINE_QUERY} LIMIT ? OFFSET ?`, [parseInt(limit,10), parseInt(offset,10)]))
             .then(res => res[0])
     }
 
     getAirlineByICAO(icao) {
-        return this.#getDBConnection().then(conn => conn.query(`SELECT * FROM airlines WHERE icao_code = ?`, [icao])) //${icao}
+        return this.#getDBConnection().then(conn => conn.query(`${AIRLINE_QUERY} WHERE icao_code = ?`, [icao]))
             .then(res => res[0])
     }
 
@@ -36,32 +46,19 @@ export class DataGateway {
 
     getFlights(offset = 0) {
         return this.#getDBConnection()
-            .then(conn => conn.query({sql: `SELECT * from realTimeFlightData as r inner join airlines a on r.airline_icao = a.icao_code
-                                                                             inner join airports arrival on r.arr_icao = arrival.icao_code
-                                                                             inner join airports departure on r.dep_icao = departure.icao_code
-                                                                             inner join countries cArr on arrival.country_code = cArr.code
-                                                                             inner join countries cDep on departure.country_code = cDep.code LIMIT 100 OFFSET ?`, nestTables: '_'}, [parseInt(offset,10)]))
+            .then(conn => conn.query({sql: `${FLIGHT_QUERY} LIMIT 100 OFFSET ?`, nestTables: '_'}, [parseInt(offset,10)]))
             .then(res => { return res[0] })
     }
 
     getFlightForId(id) {
         return this.#getDBConnection()
-            .then(conn => conn.query({sql: `SELECT * from realTimeFlightData as r inner join airlines a on r.airline_icao = a.icao_code
-                                                                             inner join airports arrival on r.arr_icao = arrival.icao_code
-                                                                             inner join airports departure on r.dep_icao = departure.icao_code
-                                                                             inner join countries cArr on arrival.country_code = cArr.code
-                                                                             inner join countries cDep on departure.country_code = cDep.code WHERE r.hex = ?`, nestTables: '_'}, [id]))
+            .then(conn => conn.query({sql: `${FLIGHT_QUERY} WHERE r.hex = ?`, nestTables: '_'}, [id]))
             .then(res => { return res[0] })
     }
 
     getFlightsForAirport(airport) {
         return this.#getDBConnection()
-            .then(conn => conn.query({sql: `SELECT * from realTimeFlightData as r inner join airlines a on r.airline_icao = a.icao_code
-                                                                             inner join airports arrival on r.arr_icao = arrival.icao_code
-                                                                             inner join airports departure on r.dep_icao = departure.icao_code
-                                                                             inner join countries cArr on arrival.country_code = cArr.code
-                                                                             inner join countries cDep on departure.country_code = cDep.code
-                                                                             WHERE r.dep_icao = ? or r.arr_icao = ?`, nestTables: '_'},[airport, airport]))
+            .then(conn => conn.query({sql: `${FLIGHT_QUERY} WHERE r.dep_icao = ? or r.arr_icao = ?`, nestTables: '_'},[airport, airport]))
             .then(res => { return res[0] })
     }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { Component, useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [selectedQuery, setSelectedQuery] = useState('Airline')
 
   const [airOffset, setAirOffset] = useState(0);
   const [airLimit, setAirLimit] = useState(5);
@@ -68,214 +69,226 @@ function App() {
 
   }
 
+  const getQueryUI = (query) => {
+      switch (query) {
+          case "Airline":
+              return <><h3>Airlines Data</h3>
+                  <div className="myInput">Enter Offset<input type="text" className="form-control" required id="k1" value={airOffset} onChange={(e) => setAirOffset(e.target.value)} /></div>
+                  <div className="myInput">Enter Limit <input type="text" className="form-control" required id="k1" value={airLimit} onChange={(e) => setAirLimit(e.target.value)} /> <br /></div>
+                  <button className="btn btn-primary" onClick={handleSubmit}>Get Airlines Data</button>
+                  {(airlineData.length > 0) ?
+                      <table className="table"><tbody>
+                      <tr>
+                          <th>Name</th>
+                          <th>ICAO Code</th>
+                          <th>IATA Code</th>
+                      </tr>
+                      {airlineData.map((item, i) => (
+                          <tr key={i}>
+                              <td>{item.name}</td>
+                              <td>{item.icaoCode}</td>
+                              <td>{item.iataCode}</td>
+                          </tr>
+                      ))}</tbody>
+                      </table>
+                      : <div></div>}
+                  <br /> <br /></>
+          case "Flight":
+              return <><h3>Flight Data</h3>
+
+                  <div className="myInput">Enter Flight Id <input className="form-control" type="text" required id="k1" value={flightId} onChange={(e) => setFlightId(e.target.value)} /></div>
+                  <br />
+                  <button className="btn btn-primary" onClick={handleSubmit2}>Get Flight Data</button>
+                  {(flightData) ? <div>
+                          <table className="table"><tbody>
+                          <tr>
+                              <th>HexCode</th>
+                              <th>Reg Number</th>
+                              <th>Flag</th>
+                              <th>Lattitude</th>
+                              <th>Longitude</th>
+                              <th>Alt</th>
+                              <th>Dir</th>
+                              <th>Speed</th>
+                              <th>VSpeed</th>
+                              <th>Squawk</th>
+                              <th>Status</th>
+                              <th>IataCode</th>
+                              <th>IcaoCode</th>
+                              <th>Flight Number</th>
+                              <th>Airline_code</th>
+                              <th>Departure_code</th>
+                              <th>Arrival_code</th>
+                          </tr>
+                          <tr>
+                              <td>{flightData.hexCode}</td>
+                              <td>{flightData.regNumber}</td>
+                              <td>{flightData.flag}</td>
+                              <td>{flightData.lat}</td>
+                              <td>{flightData.lng}</td>
+                              <td>{flightData.alt}</td>
+                              <td>{flightData.dir}</td>
+                              <td>{flightData.speed}</td>
+                              <td>{flightData.vSpeed}</td>
+                              <td>{flightData.squawk}</td>
+                              <td>{flightData.status}</td>
+                              <td>{flightData.iataCode}</td>
+                              <td>{flightData.icaoCode}</td>
+                              <td>{flightData.flightNumber}</td>
+                              <td>{flightData.airline.icaoCode}</td>
+                              <td>{flightData.departureAirport.icaoCode}</td>
+                              <td>{flightData.arrivalAirport.icaoCode}</td>
+                          </tr>
+                          </tbody></table><br />
+                          Airline Information
+                          <table className="table"><tbody>
+                          <tr>
+                              <th>Name</th>
+                              <th>IcaoCode</th>
+                              <th>IataCode</th>
+                          </tr>
+                          <tr>
+                              <td>{flightData.airline.name}</td>
+                              <td>{flightData.airline.icaoCode}</td>
+                              <td>{flightData.airline.iataCode}</td>
+                          </tr>
+                          </tbody></table><br />
+                          Departure Airport Information
+                          <table className="table"><tbody>
+                          <tr>
+                              <th>IcaoCode</th>
+                              <th>IataCode</th>
+                              <th>Name</th>
+                              <th>Lattitude</th>
+                              <th>Longitude</th>
+                              <th>Country Name</th>
+                          </tr>
+                          <tr>
+                              <td>{flightData.departureAirport.icaoCode}</td>
+                              <td>{flightData.departureAirport.iataCode}</td>
+                              <td>{flightData.departureAirport.name}</td>
+                              <td>{flightData.departureAirport.latitude}</td>
+                              <td>{flightData.departureAirport.longitude}</td>
+                              <td>{flightData.departureAirport.countryName}</td>
+                          </tr>
+                          </tbody></table><br />
+                          Arrival Airport Information
+                          <table className="table"><tbody>
+                          <tr>
+                              <th>IcaoCode</th>
+                              <th>IataCode</th>
+                              <th>Name</th>
+                              <th>Lattitude</th>
+                              <th>Longitude</th>
+                              <th>Country Name</th>
+                          </tr>
+                          <tr>
+                              <td>{flightData.arrivalAirport.icaoCode}</td>
+                              <td>{flightData.arrivalAirport.iataCode}</td>
+                              <td>{flightData.arrivalAirport.name}</td>
+                              <td>{flightData.arrivalAirport.latitude}</td>
+                              <td>{flightData.arrivalAirport.longitude}</td>
+                              <td>{flightData.arrivalAirport.countryName}</td>
+                          </tr>
+                          </tbody></table>
+                      </div>
+                      : <div></div>}
+
+                  <br /> <br /></>
+          case "Flights By Airport":
+              return <><h3>Flights By Airport</h3>
+
+                  <div className="myInput">Enter Offset <input type="text" className="form-control" required id="k1" value={flyOffset} onChange={(e) => setFlyOffset(e.target.value)} /></div>
+                  <div className="myInput">Enter Airport Code <input type="text" className="form-control" required id="k1" value={airportCode} onChange={(e) => setAirportCode(e.target.value)} /> <br /></div>
+                  <button onClick={handleSubmit3} className="btn btn-primary">Get Flight Data</button>
+                  {(flyData) ? <div>
+                          <table className="table"><tbody>
+                          <tr>
+                              <th>HexCode</th>
+                              <th>Reg Number</th>
+                              <th>Flag</th>
+                              <th>Lattitude</th>
+                              <th>Longitude</th>
+                              <th>Alt</th>
+                              <th>Dir</th>
+                              <th>Speed</th>
+                              <th>VSpeed</th>
+                              <th>Squawk</th>
+                              <th>Status</th>
+                              <th>IataCode</th>
+                              <th>IcaoCode</th>
+                              <th>Flight Number</th>
+                              <th>Airline_code</th>
+                              <th>Departure_code</th>
+                              <th>Arrival_code</th>
+                          </tr>
+                          {flyData.map((item, i) => (
+                              <tr key={i}>
+                                  <td>{item.hexCode}</td>
+                                  <td>{item.regNumber}</td>
+                                  <td>{item.flag}</td>
+                                  <td>{item.lat}</td>
+                                  <td>{item.lng}</td>
+                                  <td>{item.alt}</td>
+                                  <td>{item.dir}</td>
+                                  <td>{item.speed}</td>
+                                  <td>{item.vSpeed}</td>
+                                  <td>{item.squawk}</td>
+                                  <td>{item.status}</td>
+                                  <td>{item.iataCode}</td>
+                                  <td>{item.icaoCode}</td>
+                                  <td>{item.flightNumber}</td>
+                                  <td>{item.airline.icaoCode}</td>
+                                  <td>{item.departureAirport.icaoCode}</td>
+                                  <td>{item.arrivalAirport.icaoCode}</td>
+                              </tr>))}
+                          </tbody></table>
+                      </div>
+                      : <div></div>}
+
+                  <br /> <br /></>
+          case "Airports By Country":
+              return <><h3>Airports By Country</h3>
+
+                  <div className="myInput">Enter Country Code <input type="text" className="form-control" required id="k1" value={countryCode} onChange={(e) => setCountryCode(e.target.value)} /></div>
+                  <br />
+                  <button onClick={handleSubmit4} className="btn btn-primary">Get Airports Data</button>
+                  {(airportData) ? <div>
+                          <table className="table"><tbody>
+                          <tr>
+                              <th>IataCode</th>
+                              <th>IcaoCode</th>
+                              <th>Name</th>
+                              <th>Lattitude</th>
+                              <th>Longitude</th>
+                              <th>Country Name</th>
+                          </tr>
+                          {airportData.map((item, i) => (
+                              <tr key={i}>
+                                  <td>{item.iataCode}</td>
+                                  <td>{item.icaoCode}</td>
+                                  <td>{item.name}</td>
+                                  <td>{item.latitude}</td>
+                                  <td>{item.longitude}</td>
+                                  <td>{item.countryName}</td>
+                              </tr>))}
+                          </tbody></table>
+                      </div>
+                      : <div></div>}</>
+      }
+  }
+
   return (
     <div className="App">
       <h2>SOEN-6441 Project - Flights Information System</h2> <br />
-      <h3>Airlines Data</h3>
-      Enter Offset <input type="text" required id="k1" value={airOffset} onChange={(e) => setAirOffset(e.target.value)} />
-      Enter Limit <input type="text" required id="k1" value={airLimit} onChange={(e) => setAirLimit(e.target.value)} /> <br />
-      <button onClick={handleSubmit}>Get Airlines Data</button>
-      {(airlineData.length > 0) ?
-        <table><tbody>
-          <tr>
-            <th>Name</th>
-            <th>ICAO Code</th>
-            <th>IATA Code</th>
-          </tr>
-          {airlineData.map((item, i) => (
-            <tr key={i}>
-              <td>{item.name}</td>
-              <td>{item.icaoCode}</td>
-              <td>{item.iataCode}</td>
-            </tr>
-          ))}</tbody>
-        </table>
-        : <div></div>}
-      <br /> <br />
-
-      <h3>Flights Data (Using filght id)</h3>
-
-      Enter Flight Id <input type="text" required id="k1" value={flightId} onChange={(e) => setFlightId(e.target.value)} />
-      <br />
-      <button onClick={handleSubmit2}>Get Flight Data</button>
-      {(flightData) ? <div>
-        <table><tbody>
-          <tr>
-            <th>HexCode</th>
-            <th>Reg Number</th>
-            <th>Flag</th>
-            <th>Lattitude</th>
-            <th>Longitude</th>
-            <th>Alt</th>
-            <th>Dir</th>
-            <th>Speed</th>
-            <th>VSpeed</th>
-            <th>Squawk</th>
-            <th>Status</th>
-            <th>IataCode</th>
-            <th>IcaoCode</th>
-            <th>Flight Number</th>
-            <th>Airline_code</th>
-            <th>Departure_code</th>
-            <th>Arrival_code</th>
-          </tr>
-          <tr>
-            <td>{flightData.hexCode}</td>
-            <td>{flightData.regNumber}</td>
-            <td>{flightData.flag}</td>
-            <td>{flightData.lat}</td>
-            <td>{flightData.lng}</td>
-            <td>{flightData.alt}</td>
-            <td>{flightData.dir}</td>
-            <td>{flightData.speed}</td>
-            <td>{flightData.vSpeed}</td>
-            <td>{flightData.squawk}</td>
-            <td>{flightData.status}</td>
-            <td>{flightData.iataCode}</td>
-            <td>{flightData.icaoCode}</td>
-            <td>{flightData.flightNumber}</td>
-            <td>{flightData.airline.icaoCode}</td>
-            <td>{flightData.departureAirport.icaoCode}</td>
-            <td>{flightData.arrivalAirport.icaoCode}</td>
-          </tr>
-        </tbody></table><br />
-        Airline Information
-        <table><tbody>
-          <tr>
-            <th>Name</th>
-            <th>IcaoCode</th>
-            <th>IataCode</th>
-          </tr>
-          <tr>
-            <td>{flightData.airline.name}</td>
-            <td>{flightData.airline.icaoCode}</td>
-            <td>{flightData.airline.iataCode}</td>
-          </tr>
-        </tbody></table><br />
-        Departure Airport Information
-        <table><tbody>
-          <tr>
-            <th>IcaoCode</th>
-            <th>IataCode</th>
-            <th>Name</th>
-            <th>Lattitude</th>
-            <th>Longitude</th>
-            <th>Country Name</th>
-          </tr>
-          <tr>
-            <td>{flightData.departureAirport.icaoCode}</td>
-            <td>{flightData.departureAirport.iataCode}</td>
-            <td>{flightData.departureAirport.name}</td>
-            <td>{flightData.departureAirport.latitude}</td>
-            <td>{flightData.departureAirport.longitude}</td>
-            <td>{flightData.departureAirport.countryName}</td>
-          </tr>
-        </tbody></table><br />
-        Arrival Airport Information
-        <table><tbody>
-          <tr>
-            <th>IcaoCode</th>
-            <th>IataCode</th>
-            <th>Name</th>
-            <th>Lattitude</th>
-            <th>Longitude</th>
-            <th>Country Name</th>
-          </tr>
-          <tr>
-            <td>{flightData.arrivalAirport.icaoCode}</td>
-            <td>{flightData.arrivalAirport.iataCode}</td>
-            <td>{flightData.arrivalAirport.name}</td>
-            <td>{flightData.arrivalAirport.latitude}</td>
-            <td>{flightData.arrivalAirport.longitude}</td>
-            <td>{flightData.arrivalAirport.countryName}</td>
-          </tr>
-        </tbody></table>
-      </div>
-        : <div></div>}
-
-      <br /> <br />
-
-      <h3>Flights Data (Using airport code and offset)</h3>
-
-      Enter Offset <input type="text" required id="k1" value={flyOffset} onChange={(e) => setFlyOffset(e.target.value)} />
-      Enter Airport Code <input type="text" required id="k1" value={airportCode} onChange={(e) => setAirportCode(e.target.value)} /> <br />
-      <button onClick={handleSubmit3}>Get Flight Data</button>
-      {(flyData) ? <div>
-        <table><tbody>
-          <tr>
-            <th>HexCode</th>
-            <th>Reg Number</th>
-            <th>Flag</th>
-            <th>Lattitude</th>
-            <th>Longitude</th>
-            <th>Alt</th>
-            <th>Dir</th>
-            <th>Speed</th>
-            <th>VSpeed</th>
-            <th>Squawk</th>
-            <th>Status</th>
-            <th>IataCode</th>
-            <th>IcaoCode</th>
-            <th>Flight Number</th>
-            <th>Airline_code</th>
-            <th>Departure_code</th>
-            <th>Arrival_code</th>
-          </tr>
-          {flyData.map((item, i) => (
-            <tr key={i}>
-              <td>{item.hexCode}</td>
-              <td>{item.regNumber}</td>
-              <td>{item.flag}</td>
-              <td>{item.lat}</td>
-              <td>{item.lng}</td>
-              <td>{item.alt}</td>
-              <td>{item.dir}</td>
-              <td>{item.speed}</td>
-              <td>{item.vSpeed}</td>
-              <td>{item.squawk}</td>
-              <td>{item.status}</td>
-              <td>{item.iataCode}</td>
-              <td>{item.icaoCode}</td>
-              <td>{item.flightNumber}</td>
-              <td>{item.airline.icaoCode}</td>
-              <td>{item.departureAirport.icaoCode}</td>
-              <td>{item.arrivalAirport.icaoCode}</td>
-            </tr>))}
-        </tbody></table>
-        </div>
-        : <div></div>}
-
-        <br /> <br />
-
-        <h3>Airports Data</h3>
-
-        Enter Country Code <input type="text" required id="k1" value={countryCode} onChange={(e) => setCountryCode(e.target.value)} />
-         <br />
-        <button onClick={handleSubmit4}>Get Airports Data</button>
-        {(airportData) ? <div>
-          <table><tbody>
-            <tr>
-              <th>IataCode</th>
-              <th>IcaoCode</th>
-              <th>Name</th>
-              <th>Lattitude</th>
-              <th>Longitude</th>
-              <th>Country Name</th>
-            </tr>
-            {airportData.map((item, i) => (
-              <tr key={i}>
-                <td>{item.iataCode}</td>
-                <td>{item.icaoCode}</td>
-                <td>{item.name}</td>
-                <td>{item.latitude}</td>
-                <td>{item.longitude}</td>
-                <td>{item.countryName}</td>
-              </tr>))}
-          </tbody></table>
-        </div>
-          : <div></div>}
-
-      </div>
+      <select value={selectedQuery} onChange={e => setSelectedQuery(e.target.value)} className="form-select">
+          <option value="Airline">Airline</option>
+          <option value="Flight">Flight</option>
+          <option value="Flights By Airport">Flights By Airport</option>
+          <option value="Airports By Country">Airports By Country</option>
+      </select>
+        {getQueryUI(selectedQuery)}
+    </div>
   );
 }
 
